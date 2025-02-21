8049a9526bc76293591b1d574e3f41fa4869b4f4
seheriremunal <seheriremunal@gmail.com>
2025-02-21 11:35:51 +0300

    endpoint düzenlendi

--- src/main/java/org/hrmplatform/hrmplatform/constant/EndPoints.java
+++ src/main/java/org/hrmplatform/hrmplatform/constant/EndPoints.java
@@ -1,18 +1,19 @@
 package org.hrmplatform.hrmplatform.constant;
 
 public class EndPoints {
-	
+
 	public static final String VERSION = "/v1";
-	
+
 	public static final String API = "/api";
 	public static final String DEVELOPER = "/dev";
 	public static final String TEST = "/test";
 	public static final String PROD = "/prod";
-	
+
 	public static final String ROOT = VERSION + DEVELOPER;
-	
+
 	public static final String USER = ROOT + "/user";
 	public static final String USERROLE =ROOT+ "/userrole";
+	public static final String LEAVE =ROOT+ "/leave";
 
 	public static final String AUTH = API + "/auth"; // Base path olarak auth eklendi
 
@@ -24,6 +25,7 @@ public class EndPoints {
 	public static final String FORGOTPASSWORD = "/forgot-password";
 	public static final String RESETPASSWORD = "/reset-password";
 
+
 	//USERMANAGEMENT
 	public static final String PROFILE = "/{userId}";
 	public static final String DELETE = "/{userId}";
@@ -31,7 +33,8 @@ public class EndPoints {
 	public static final String STATUS = "/{userId}/status";
 	public static final String UPDATE = "/update";
 	public static final String FINDALL_USERS = "/findAll-users";
-	
+
+
 	//USERROLE
 	public static final String ASSIGNROLES = "/assign-roles";
 	public static final String FINDALL = "/findall";
@@ -39,6 +42,14 @@ public class EndPoints {
 	public static final String SEARCHBYNAME = "/searchByName";
 	public static final String SEARCHBYROLE = "/searchByRole";
 	public static final String DELETEUSERROLE = "/userrole/delete/{userId}";
+
+	//LEAVEREQUEST
+
+	public static final String LEAVEREQUEST = "/leaverequest"; // Kullanıcı yeni izin talebi oluşturur
+	public static final String LEAVEBYUSERID = "/leavebyuserid/{employeeId}";
+	public static final String PENDINGLEAVESFORMANAGER = "/manager/{managerId}/pending-leaves"; // Yönetici, bekleyen izin taleplerini görüntüler
+	public static final String ACCEPTLEAVE = "/manager/{managerId}/approve/{leaveRequestId}"; // Yönetici, izin talebini onaylar
+	public static final String REJECTLEAVE = "/manager/{managerId}/reject/{leaveRequestId}"; // Yönetici, izin talebini reddeder
 
 	public static final String COMPANY = "/company";
 	public static final String FINDALLCOMPANY =COMPANY+ "/find-all-company";

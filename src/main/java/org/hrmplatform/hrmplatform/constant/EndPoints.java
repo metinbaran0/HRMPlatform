d41618200c6139011319f8d3911c56c87646ce37
visalatas <vslats66@gmail.com>
2025-02-27 10:39:16 +0300

    ErrorType

--- src/main/java/org/hrmplatform/hrmplatform/constant/EndPoints.java
+++ src/main/java/org/hrmplatform/hrmplatform/constant/EndPoints.java
@@ -1,6 +1,7 @@
 package org.hrmplatform.hrmplatform.constant;
 
 public class EndPoints {
+
 	
 	public static final String VERSION = "/v1";
 	
@@ -11,14 +12,14 @@ public class EndPoints {
 	
 	public static final String ROOT = VERSION + API;
 	
-	
 	public static final String USER = ROOT + "/user";
 	public static final String USERROLE =ROOT+ "/userrole";
 
 	public static final String LEAVE =ROOT+ "/leave";
 	
 	public static final String AUTH = API + "/auth"; // Base path olarak auth eklendi
-	
+
+
 	// USER
 	public static final String REGISTER = "/register";
 	public static final String DOLOGIN = "/dologin";
@@ -26,6 +27,7 @@ public class EndPoints {
 	public static final String RESENDACTIVATIONEMAIL = "/resend-activation-email";
 	public static final String FORGOTPASSWORD = "/forgot-password";
 	public static final String RESETPASSWORD = "/reset-password";
+	
 
 	//USERMANAGEMENT
 	public static final String PROFILE = "/{userId}";
@@ -45,7 +47,6 @@ public class EndPoints {
 	public static final String SEARCHBYROLE = "/searchByRole";
 	public static final String DELETEUSERROLE = "/userrole/delete/{userId}";
 
-	//COMPANY
 	public static final String COMPANY = "/company";
 	public static final String FINDALLCOMPANY =COMPANY+ "/find-all-company";
 	public static final String FINDBYCOMPANYID =COMPANY+ "/find-byid-company";
@@ -56,14 +57,17 @@ public class EndPoints {
 	public static final String APPROVE =COMPANY+ "/approve-company";
 	public static final String REJECT =COMPANY+ "/reject-company";
 	
-		//LEAVEREQUEST
+	
+	//LEAVEREQUEST
 	
 	public static final String LEAVEREQUEST = "/leaverequest"; // Kullanıcı yeni izin talebi oluşturur
 	public static final String LEAVEBYUSERID = "/leavebyuserid/{employeeId}"; //kullanıcın kendi izinlerini getirme
 	public static final String PENDINGLEAVESFORMANAGER = "/manager/{managerId}/pending-leaves"; // Yönetici, bekleyen izin taleplerini görüntüler
 	public static final String ACCEPTLEAVE = "/manager/{managerId}/approve/{employeeId}"; // Yönetici, izin talebini onaylar
 	public static final String REJECTLEAVE = "/manager/{managerId}/reject/{employeeId}"; // Yönetici, izin talebini reddeder
-  	// EMPLOYEE
+	
+
+	// EMPLOYEE
 	public static final String EMPLOYEE = ROOT + "/employee";
 	public static final String GET_ALL_EMPLOYEES = "/get-all-employees";
 	public static final String CREATE_EMPLOYEE = "/create-employee";
@@ -71,16 +75,10 @@ public class EndPoints {
 	public static final String DELETE_EMPLOYEE =  "delete-employee/{id}";
 	public static final String CHANGE_EMPLOYEE_STATUS =  "change-/{id}/status";
 
+
+
+
 }
-
-
-	
-
-
-
-
-
-
 
 
 

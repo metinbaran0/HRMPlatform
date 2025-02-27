8b9bc7846efc693698656b3ab16b214f8381a98e
seheriremunal <seheriremunal@gmail.com>
2025-02-27 10:27:24 +0300

    hatalar düzeltildi

--- src/main/java/org/hrmplatform/hrmplatform/constant/EndPoints.java
+++ src/main/java/org/hrmplatform/hrmplatform/constant/EndPoints.java
@@ -75,47 +75,9 @@ public class EndPoints {
 	public static final String DELETE_EMPLOYEE =  "delete-employee/{id}";
 	public static final String CHANGE_EMPLOYEE_STATUS =  "change-/{id}/status";
 
-	//LEAVEREQUEST
 
-	public static final String LEAVEREQUEST = "/leaverequest"; // Kullanıcı yeni izin talebi oluşturur
-	public static final String LEAVEBYUSERID = "/leavebyuserid/{employeeId}";
-	public static final String PENDINGLEAVESFORMANAGER = "/manager/{managerId}/pending-leaves"; // Yönetici, bekleyen izin taleplerini görüntüler
-	public static final String ACCEPTLEAVE = "/manager/{managerId}/approve/{leaveRequestId}"; // Yönetici, izin talebini onaylar
-	public static final String REJECTLEAVE = "/manager/{managerId}/reject/{leaveRequestId}"; // Yönetici, izin talebini reddeder
 
 }
 
 
 
-
-//package com.barisd.java16_x.constant;
-//
-//public class Endpoints {
-//
-//	public static final String VERSION="/v1";
-//
-//	public static final String API="/api";
-//	public static final String DEVELOPER="/dev";
-//	public static final String TEST="/test";
-//	public static final String PROD="/prod";
-//
-//	public static final String ROOT=VERSION+DEVELOPER;
-//
-//	public static final String USER= ROOT+"/user";
-//	public static final String POST= ROOT+"/post";
-//	public static final String USERYETKI= ROOT+"/useryetki";
-//
-//	//USER
-//	public static final String SAVE="/save";
-//	public static final String FINDALL="/findall";
-//	public static final String FINDBYUSERID="/findbyuserid";
-//	public static final String REGISTER="/register";
-//	public static final String DOLOGIN="/dologin";
-//	public static final String GETPROFILE="/get-profile";
-//	//POST
-//	public static final String CREATEPOST="/create-post";
-//	public static final String GETUSERSALLPOST="/get-users-all-post";
-//	public static final String GETALLPOST="/get-all-post";
-//
-//
-//}
--- src/main/java/org/hrmplatform/hrmplatform/exception/ErrorType.java
+++ src/main/java/org/hrmplatform/hrmplatform/exception/ErrorType.java
@@ -22,9 +22,9 @@ public enum ErrorType implements ErrorTypeInterface {
 
 	ACTIVATION_CODE_EXPIRED(456,"Etkinleştirme kodunun süresi doldu" , HttpStatus.BAD_REQUEST ),
 	USER_ALREADY_ACTIVATED(123, "Kullanıcı zaten aktif", HttpStatus.BAD_REQUEST),
-	USER_EMAIL_NOT_VERIFIED(753 ,"Kullanıcı mail onaylanmamış",HttpStatus.BAD_REQUEST );
+	USER_EMAIL_NOT_VERIFIED(753 ,"Kullanıcı mail onaylanmamış",HttpStatus.BAD_REQUEST ),
 
-	UNAUTHORIZED_OPERATION(403, "Bu işlemi yapmak için yetkiniz yok.", HttpStatus.FORBIDDEN);
+	UNAUTHORIZED_OPERATION(403, "Bu işlemi yapmak için yetkiniz yok.", HttpStatus.FORBIDDEN),
 
 	EMPLOYEE_NOT_FOUND(404, "Çalışan bulunamadı", HttpStatus.NOT_FOUND);
 

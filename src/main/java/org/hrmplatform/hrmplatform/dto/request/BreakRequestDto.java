a02bef9a65d92df17a3253c04ca775b97740a698
seheriremunal <seheriremunal@gmail.com>
2025-03-10 23:37:59 +0300

    break metotları token ile yapıldı

--- src/main/java/org/hrmplatform/hrmplatform/dto/request/BreakRequestDto.java
+++ src/main/java/org/hrmplatform/hrmplatform/dto/request/BreakRequestDto.java
@@ -1,12 +1,18 @@
 package org.hrmplatform.hrmplatform.dto.request;
 
-import java.time.LocalDate;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public record BreakRequestDto(
         Long shiftId,
-        Long companyId,
         String breakName,
-        LocalDate startTime,
-        LocalDate endTime
+        LocalDateTime startTime,
+        LocalDateTime endTime
 ) {
 }
